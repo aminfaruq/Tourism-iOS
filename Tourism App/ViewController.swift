3232b337b2340ed7eb4a9c000ace011aadfb1772
mujibbur741 <mujiburr741@gmail.com>
2021-11-26 13:44:06 +0700

    Initial Commit

---
+++ Tourism App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tourism App
+//
+//  Created by Mujibbur 741 on 26/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tourism AppUITests/Tourism_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Tourism_AppUITestsLaunchTests.swift
+//  Tourism AppUITests
+//
+//  Created by Mujibbur 741 on 26/11/21.
+//
+
+import XCTest
+
+class Tourism_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
